b894538c1d0bd20cc1545c49f8332804e76485f9
Donnie Brown <donnie@Brown.local>
2013-11-14 10:31:00 -0500

    Initial C

---
+++ C/helloworld.c
@@ -0,0 +1,18 @@
+//
+// hello.c
+// Hello World
+//
+// Created by Donald Brown on 20130913
+// Copyright (c) 2013 Donald Brown. All rights reserved.
+//
+
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+	// Output "Hello, World!" and create a new line.
+	printf("Hello, World!\n");
+	return 0;
+}
